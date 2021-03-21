1fd99663839e71c461af68799fd19b49fc04439e
gottagofaster236 <lr.soft.now@gmail.com>
2021-03-21 21:11:42 +0300

    Implemented getVideoDevicesListWithDuplicates

--- SonicVisualSplitBase/VirtualCamCapture.cpp
+++ SonicVisualSplitBase/VirtualCamCapture.cpp
@@ -1,9 +1,13 @@
 #include "VirtualCamCapture.h"
+#include <dshow.h>
 
 namespace SonicVisualSplitBase {
 
-VirtualCamCapture::VirtualCamCapture(int cameraIndex) {
-    videoCapture = cv::VideoCapture(cameraIndex, cv::CAP_DSHOW);
+VirtualCamCapture::VirtualCamCapture(int deviceIndex) {
+    videoCapture = cv::VideoCapture(deviceIndex, cv::CAP_DSHOW);
+    // Set the highest possible resolution. (Assuming it's less than 32000 pixels).
+    videoCapture.set(cv::CAP_PROP_FRAME_WIDTH, 32000);
+    videoCapture.set(cv::CAP_PROP_FRAME_HEIGHT, 32000);
 }
 
 
@@ -18,6 +22,97 @@ cv::Mat VirtualCamCapture::captureRawFrame() {
 
 VirtualCamCapture::~VirtualCamCapture() {
     videoCapture.release();
+}
+
+
+// Code for device enumeration taken from here: https://docs.microsoft.com/en-us/windows/win32/directshow/selecting-a-capture-device
+static HRESULT EnumerateDevices(REFGUID category, IEnumMoniker** ppEnum) {
+    // Create the System Device Enumerator.
+    ICreateDevEnum* pDevEnum;
+    HRESULT hr = CoCreateInstance(CLSID_SystemDeviceEnum, NULL,
+                                  CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pDevEnum));
+
+    if (SUCCEEDED(hr)) {
+        // Create an enumerator for the category.
+        hr = pDevEnum->CreateClassEnumerator(category, ppEnum, 0);
+        if (hr == S_FALSE)         {
+            hr = VFW_E_NOT_FOUND;  // The category is empty. Treat as an error.
+        }
+        pDevEnum->Release();
+    }
+    return hr;
+}
+
+
+std::vector<std::wstring> VirtualCamCapture::getVideoDevicesListWithDuplicates() {
+    if (!hasInitializedCom) {
+        HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+        if (FAILED(hr))
+            return {};
+        hasInitializedCom = true;
+    }
+
+    std::vector<std::wstring> devices;
+    IEnumMoniker* pEnum;
+    HRESULT hr = EnumerateDevices(CLSID_VideoInputDeviceCategory, &pEnum);
+
+    if (SUCCEEDED(hr)) {
+        IMoniker* pMoniker = NULL;
+
+        while (pEnum->Next(1, &pMoniker, NULL) == S_OK) {
+            IPropertyBag* pPropBag;
+            HRESULT hr = pMoniker->BindToStorage(0, 0, IID_PPV_ARGS(&pPropBag));
+            auto failString = L"Unknown";
+
+            if (FAILED(hr)) {
+                pMoniker->Release();
+                devices.push_back(failString);
+                continue;
+            }
+
+            VARIANT var;
+            VariantInit(&var);
+            // Get the friendly name.
+            hr = pPropBag->Read(L"FriendlyName", &var, 0);
+            if (SUCCEEDED(hr)) {
+                devices.push_back(std::wstring(var.bstrVal));
+                VariantClear(&var);
+            }
+            else {
+                devices.push_back(failString);
+            }
+
+            pPropBag->Release();
+            pMoniker->Release();
+        }
+
+        pEnum->Release();
+    }
+    return devices;
+}
+
+
+std::vector<std::wstring> VirtualCamCapture::getVideoDevicesList() {
+    // Removing the duplicates by adding the duplicate number.
+    std::vector<std::wstring> devicesWithDuplicates = getVideoDevicesListWithDuplicates();
+    std::vector<std::wstring> devices;
+
+    for (const std::wstring& name : devicesWithDuplicates) {
+        std::wstring addName = name;
+        int repetition = 1;
+        while (std::find(devices.begin(), devices.end(), addName) != devices.end()) {
+            repetition++;
+            addName = name + L" (" + std::to_wstring(repetition) + L")";
+        }
+        devices.push_back(addName);
+    }
+    return devices;
+}
+
+
+void VirtualCamCapture::uninitializeCom() {
+    CoUninitialize();
+    hasInitializedCom = false;
 }
 
 }  // namespace SonicVisualSplitBase
--- SonicVisualSplitBase/VirtualCamCapture.h
+++ SonicVisualSplitBase/VirtualCamCapture.h
@@ -3,21 +3,30 @@
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 
-
 namespace SonicVisualSplitBase {
 
 class VirtualCamCapture : public GameVideoCapture {
 public:
     // Initializes the capture with a camera index.
-    VirtualCamCapture(int cameraIndex);
+    VirtualCamCapture(int deviceIndex);
 
     // Grabs a frame from the video capture, or returns an empty Mat on error.
     cv::Mat captureRawFrame() override;
 
     ~VirtualCamCapture() override;
 
+    static std::vector<std::wstring> getVideoDevicesList();
+
+    // Should be called to cleanup the COM library, that is used by getVideoDevicesList.
+    static void uninitializeCom();
+
 private:
     cv::VideoCapture videoCapture;
+
+    // Returns the list of device names, which may contain duplicate names.
+    static std::vector<std::wstring> getVideoDevicesListWithDuplicates();
+
+    static inline bool hasInitializedCom = false;
 };
 
 }  // namespace SonicVisualSplitBase

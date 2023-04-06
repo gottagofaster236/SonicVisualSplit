98a232192c8c61c13faba7f67a3b702e95746456
gottagofaster236 <lr.soft.now@gmail.com>
2023-04-06 02:19:11 +0200

    Update the version.

--- SonicVisualSplit/Properties/AssemblyInfo.cs
+++ SonicVisualSplit/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SonicVisualSplit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SonicVisualSplit component for LiveSplit.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("gottagofaster236")]
 [assembly: AssemblyProduct("SonicVisualSplit")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.5")]
-[assembly: AssemblyFileVersion("1.2.5")]
+[assembly: AssemblyVersion("1.2.6")]
+[assembly: AssemblyFileVersion("1.2.6")]
